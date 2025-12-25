3c4654981ed415e6fadd673357be10c1835449d8
Josiah Welch <jwelch11@huskers.unl.edu>
2025-12-24 23:05:39 -0600

    fixed buffer comparision

--- src/main.c
+++ src/main.c
@@ -13,17 +13,20 @@ int main() {
 	int acct_creation_loop = 1;
 
 	while (acct_input_loop) {
-		printf("Input account id: ");
+		printf("\nInput account id: ");
+		memset(acct, '\0', sizeof(acct)); // Clears buffer
 		scanf("%s", &acct);
 		sprintf(acct_addr, "~/.config/payroll-cli/db/%s.cfg");
 		f_ptr = fopen(acct_addr, "r");
 		if (f_ptr == NULL) {
 			printf("\nThat account was not found! Would you like to create it? [Y/N]: ");
 			while (acct_creation_loop) {
+				memset(buf, '\0', sizeof(buf)); // Clears buffer
 				scanf("%s", &buf);
 				acct_creation_loop = 0;
-				if (buf == 'Y' || buf == 'y') {
+				if (buf[0] == 'Y' || buf[0] == 'y') {
 					printf("\nEnter the name of the account: ");
+					memset(acct_name, '\0', sizeof(acct_name)); // Clears buffer
 					scanf("%s", &acct_name);
 				}
 			}
